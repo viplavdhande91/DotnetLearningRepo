1905da30bd5414ef7687566653cd1db573f75e9f
Viplav Patil <viplav.patil@nagarro.com>
2023-05-26 12:58:20 +0530

    changes in ef core

--- Controllers/PersonController.cs
+++ Controllers/PersonController.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using dotnet_project.Data;
 using dotnet_project.Models;
+using Microsoft.Data.SqlClient;
 
 namespace dotnet_project.Controllers
 {
@@ -28,18 +29,43 @@ namespace dotnet_project.Controllers
             return await _context.Persons_.ToListAsync();
         }
 
-        // GET: api/Person/5
-        [HttpGet("{id}")]
-        public async Task<ActionResult<Person>> GetPerson(Guid id)
+        //// GET: api/Person/5
+        //[HttpGet("{id}")]
+        //public async Task<ActionResult<Person>> GetPerson(Guid id)
+        //{
+        //    var person = await _context.Persons_.FindAsync(id);
+
+        //    if (person == null)
+        //    {
+        //        return NotFound();
+        //    }
+
+        //    return person;
+        //}
+
+        [HttpGet("SP/{personId}")] //STORED PROCEDURE WITH  PARAMETERS
+        public async Task<ActionResult<List<Person>>> GetCharacters(Guid personId)
         {
-            var person = await _context.Persons_.FindAsync(id);
+            
 
-            if (person == null)
-            {
-                return NotFound();
-            }
 
-            return person;
+            var result = await _context.Persons_.FromSqlRaw("EXEC SelectSpecificPerson {0}", personId.ToString()).ToListAsync();
+             
+            return Ok(result);
+
+            
+        
+
+
+        }
+
+
+        [HttpGet("SP")] //STORED PROCEDURE WITH NO PARAMETERS
+        public async Task<ActionResult<List<Person>>> GetAllCharactersSP()
+        {
+            var result = await _context.Persons_.FromSqlRaw("SelectAllPersons").ToListAsync();
+
+            return Ok(result);
         }
 
         // PUT: api/Person/5
