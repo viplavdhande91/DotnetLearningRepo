e493abddec52bf8fb3b23e6c306f23ab69236be0
Viplav Patil <viplav.patil@alfalaval.com>
2024-08-19 17:17:53 +0530

    .NET v8 Upgraded

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
 
 namespace dotnet_project.Controllers
 {
@@ -25,14 +20,12 @@ namespace dotnet_project.Controllers
 
         [HttpGet]
         public IEnumerable<WeatherForecast> Get()
-        { 
-            var request = Request.Cookies.FirstOrDefault();
-            var rng = new Random();
+        {
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
-                Date = DateTime.Now.AddDays(index),
-                TemperatureC = rng.Next(-20, 55),
-                Summary = Summaries[rng.Next(Summaries.Length)]
+                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
             })
             .ToArray();
         }
